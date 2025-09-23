94c2a00a63d6a23b28b9137f3b1833e45093f339
Reda-islam <rayhanalfna@gmail.com>
2025-09-23 09:42:30 +0100

    Update MainActivity.kt

--- app/src/main/java/com/masafir/app/MainActivity.kt
+++ app/src/main/java/com/masafir/app/MainActivity.kt
@@ -1,32 +1,53 @@
-package com.example.masafir
+package com.masafir.app
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val myWebView = WebView(this)
+        webView = WebView(this)
+        setContentView(webView)
 
-        // باش يبقى التصفح داخل التطبيق وما يخرجش للبراوزر
-        myWebView.webViewClient = WebViewClient()
+        // WebView settings
+        with(webView.settings) {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            loadsImagesAutomatically = true
+            allowFileAccess = true
+            databaseEnabled = true
+            cacheMode = WebSettings.LOAD_DEFAULT
+            mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+        }
 
-        val webSettings: WebSettings = myWebView.settings
-        webSettings.javaScriptEnabled = true
-        webSettings.domStorageEnabled = true
-        webSettings.allowFileAccess = true
-        webSettings.loadsImagesAutomatically = true
-        webSettings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
 
-        // رابط Netlify ديالك
-        myWebView.loadUrl("https://mellifluous-douhua-9377eb.netlify.app")
+        // 🔹 حط هنا دومين Netlify ديالك
+        webView.loadUrl("https://mellifluous-douhua-9377eb.netlify.app/")
 
-        setContentView(myWebView)
+        // رجوع للخلف داخل الويب ڤيو
+        onBackPressedDispatcher.addCallback(this,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    if (webView.canGoBack()) webView.goBack() else finish()
+                }
+            })
+    }
+
+    override fun onDestroy() {
+        webView.destroy()
+        super.onDestroy()
     }
 }
